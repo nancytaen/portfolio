f04e5ddb3917256fa0a002dd2277b796657e8217
nancytaen <taen.heizo8@gmail.com>
2020-07-31 19:59:54 -0400

    added bootstrap

---
+++ src/components/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h2 mb-0">
+    <b-icon-arrow-up></b-icon-arrow-up>
+    <b-icon-exclamation-triangle-fill></b-icon-exclamation-triangle-fill>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Layout'
+}
+</script>
