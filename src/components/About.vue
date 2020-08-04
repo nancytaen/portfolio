53aa2900f7f1e00e7dcc89e5153167e958169ce1
nancytaen <taen.heizo8@gmail.com>
2020-08-03 20:41:12 -0400

    download resume

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,20 +1,22 @@
 <template>
-    <div>
+    <div class="about-me">
       <div>
         <!-- header & image -->
       <h1>
         Nancy Li
       </h1>
       <b-img src="img/profile.jpg" rounded="circle" fluid alt="Picture of Nancy"/>
-      <!-- first body paragraph -->
-      <div>
-        Hello! My name is Nancy Li. I am a 3rd year student studying Computer Engineering at University of Toronto. 
+      <div class="about-me-body">
+        <!-- first body paragraph -->
+        <div>
+          Hello! My name is Nancy Li. I am a 3rd year student studying Computer Engineering at University of Toronto. 
 
-      </div>
-      <!-- second body paragraph -->
-      <div></div>
-      <!-- download resume -->
-      <a href="file/NancyLi_Resume.pdf" target="_blank">Resume </a>
+        </div>
+        <!-- second body paragraph -->
+        <div></div>
+        <!-- download resume -->
+        <a href="file/NancyLi_Resume.pdf" target="_blank">Resume </a>
+        </div>
       </div>
   </div>
 </template>
