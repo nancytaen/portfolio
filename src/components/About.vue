d2fe9e7effd36424b28d90d29b63ba379dd83317
nancytaen <taen.heizo8@gmail.com>
2020-08-12 20:58:56 -0400

    about me paragraph

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,13 +4,22 @@
         <!-- header & image -->
       <b-img src="img/profile.jpg" rounded="circle" fluid alt="Picture of Nancy"/>
       <div class="about-me-body">
-        <!-- first body paragraph -->
-        <div>
-          Hello! My name is Nancy Li. I am a 3rd year student studying Computer Engineering at University of Toronto. 
+        <div class="col-sm-11">
+          <br>
 
+          <p class="lead"> Hello! My name is Nancy Li.</p> 
+          I am a <strong> 3rd year Computer Engineering </strong> student studying at University of Toronto. 
+          My passion lies in <u> computer software, hardware, and mathematics </u>. I am especially interested in software development, 
+          including <u> web development and machine learning </u>. I enjoy exploring new algorithms and technologies, and am constantly looking for 
+          methods to improve the <strong> efficiency </strong> of the program. I also value <strong> organization </strong> and <strong> readability </strong> when coding. I put strong emphasis on 
+          neat project structure, logical classes and database models, and detailed documentation. <br>
+
+          <br>
+
+          In school, I am also taking courses on <u> mathematics, computer hardware, and business </u>, which I am looking  
+          to apply in software development, such as in machine learning and financial engineering. I am hoping to use my skills and knowledge 
+          to build software that can fill <em>the gap</em> in people's lives. 
         </div>
-        <!-- second body paragraph -->
-        <div></div>
         <!-- download resume -->
         <a href="file/NancyLi_Resume.pdf" target="_blank">Resume </a>
         </div>
