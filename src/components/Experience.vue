a61a6c46c6a088558ee71ead052fb7e523eafeb5
nancytaen <taen.heizo8@gmail.com>
2020-08-19 17:11:45 -0400

    work experience section formatting

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,8 +1,26 @@
 <template>
     <div>
-        <h2>Work Experience</h2>
+        <div class="mt-4 mb-5">
+            <h3>Work Experience</h3>
+            <b-card v-for="job in jobs" :key="job.title" :title="job.title" class="col-sm-9 mx-auto text-left mb-2">
+                {{job.company}} | {{job.date}} | {{job.location}}
+                <br>
+                <ul>
+                    <li v-for="detail in job.details" :key="detail">{{detail}}</li>
+                </ul>
+            </b-card>
+        </div>
 
-        <h2>Entrepreneurial Experience</h2>
+        <div>
+            <h3>Entrepreneurial Experience</h3>
+            <b-card v-for="entrep in entreps" :key="entrep.name" :title="entrep.name" class="col-sm-9 mx-auto text-left mb-2">
+                {{entrep.project}} | {{entrep.date}}
+                <br>
+                <ul>
+                    <li v-for="detail in entrep.details" :key="detail">{{detail}}</li>
+                </ul>
+            </b-card>
+        </div>
     </div>
 </template>
 
@@ -11,8 +29,16 @@ export default {
     name: 'Experience',
     data: function() {
         return {
-            jobs: [],
-            entrep: [],
+            jobs: [
+                {title: 'Full Stack Developer Intern', company: 'Uniaim Co., Ltd.', location: 'Tokyo, Japan',
+                 date: 'May - August 2019', details: []},
+                ],
+            entreps: [
+                {name: 'MoneyPong', project: 'University of Toronto Hatchery', date: 'September 2019 - June 2020',
+                 details: []},
+                {name: 'Givt', project: 'University of Toronto Hatchery', date: 'June 2020 - present', 
+                 details: []}
+                ],
         }
     }
 }
