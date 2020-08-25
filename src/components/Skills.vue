cb0889a04f0a77d0d144eb2d3c504d5907c71ae2
nancytaen <taen.heizo8@gmail.com>
2020-08-25 12:14:09 -0400

    added icons in skills section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -5,39 +5,20 @@
         <div class="mb-4">
             <h4>Programming</h4>
             <div class="container-fluid">
-            <div class="row justify-content-center">
-                <b-card v-for="(skill, idx) in skills.program" :key="idx"
-                class="col-lg-2 col-sm-4 mr-3 mb-3 pr-0 pl-0"
-                border-variant="warning" :header="skill.name"
-                header-bg-variant="transparent"
-                align="center">
-                <template v-slot:footer>
-                    <span v-for="exp in skill.exps" :key="exp">
-                    <small class="text-muted">{{exp}}</small><br>
-                    </span>
-                </template>
-                </b-card>
-            </div>
-            </div>
-        </div>
-
-        <!-- Hardware Skills -->
-        <div class="mt-2 mb-4">
-            <h4>Hardware</h4>
-            <div class="container">
-            <div class="row justify-content-center">
-                <b-card v-for="(skill, idx) in skills.hardware" :key="idx"
-                class="col-lg-2 col-sm-4 mr-3 mb-3 pr-0 pl-0"
-                border-variant="success" :header="skill.name"
-                header-bg-variant="transparent"
-                align="center">
-                <template v-slot:footer>
-                    <span v-for="exp in skill.exps" :key="exp">
-                    <small class="text-muted">{{exp}}</small><br>
-                    </span>
-                </template>
-                </b-card>
-            </div>
+                <div class="row justify-content-center">
+                    <b-card v-for="(skill, idx) in skills.program" :key="idx"
+                    class="col-lg-2 col-sm-3 mr-3 mb-3 pr-0 pl-0"
+                    border-variant="warning" :header="skill.name"
+                    header-bg-variant="transparent"
+                    align="center">
+                    <b-card-img :src="skill.img" class="prog-logo" />
+                    <template v-slot:footer>
+                        <span v-for="exp in skill.exps" :key="exp">
+                        <small class="text-muted">{{exp}}</small><br>
+                        </span>
+                    </template>
+                    </b-card>
+                </div>
             </div>
         </div>
 
@@ -45,19 +26,41 @@
         <div class="mt-2 mb-4">
             <h4>System</h4>
             <div class="container">
-            <div class="row justify-content-center">
-                <b-card v-for="(skill, idx) in skills.system" :key="idx"
-                class="col-lg-2 col-sm-4 mr-3 mb-3 pr-0 pl-0"
-                border-variant="info" :header="skill.name"
-                header-bg-variant="transparent"
-                align="center">
-                <template v-slot:footer>
-                    <span v-for="exp in skill.exps" :key="exp">
-                    <small class="text-muted">{{exp}}</small><br>
-                    </span>
-                </template>
-                </b-card>
+                <div class="row justify-content-center">
+                    <b-card v-for="(skill, idx) in skills.system" :key="idx"
+                    class="col-lg-2 col-sm-4 mr-3 mb-3 pr-0 pl-0"
+                    border-variant="info" :header="skill.name"
+                    header-bg-variant="transparent"
+                    align="center">
+                    <b-card-img :src="skill.img" class="prog-logo" />
+                    <template v-slot:footer>
+                        <span v-for="exp in skill.exps" :key="exp">
+                        <small class="text-muted">{{exp}}</small><br>
+                        </span>
+                    </template>
+                    </b-card>
+                </div>
             </div>
+        </div>
+
+        <!-- Hardware Skills -->
+        <div class="mt-2 mb-4">
+            <h4>Hardware</h4>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <b-card v-for="(skill, idx) in skills.hardware" :key="idx"
+                    class="col-lg-4 col-sm-8 mr-3 mb-3 pr-0 pl-0"
+                    border-variant="success" :header="skill.name"
+                    header-bg-variant="transparent"
+                    align="center">
+                    <b-card-img :src="skill.img" class="hd-logo" />
+                    <template v-slot:footer>
+                        <span v-for="exp in skill.exps" :key="exp">
+                        <small class="text-muted">{{exp}}</small><br>
+                        </span>
+                    </template>
+                    </b-card>
+                </div>
             </div>
         </div>
 
@@ -71,20 +74,31 @@ export default {
     return {
       skills: {
         program: [
-          {name: 'Python', exps: ['6 years learning', '4 months intern']},
-          {name: 'C++', exps: ['1 year learning']},
-          {name: 'C', exps: ['2 years learning']},
-          {name: 'VueJS', exps: ['1 year learning', '4 months intern']},
-          {name: 'HTML', exps: ['1 year learning', '4 months intern']},
-          {name: 'Bootstrap', exps: ['1 year learning', '4 months intern']}
+          {name: 'Python', exps: ['6 years learning', '4 months intern'], img:'img/logos/python.png'},
+          {name: 'C++', exps: ['1 year learning'], img:'img/logos/cplusplus.png'},
+          {name: 'C', exps: ['2 years learning'], img:'img/logos/c.png'},
+          {name: 'VueJS', exps: ['1 year learning', '4 months intern'], img:'img/logos/vue.png'},
+          {name: 'HTML', exps: ['1 year learning', '4 months intern'], img:'img/logos/html.png'},
+          {name: 'Bootstrap', exps: ['1 year learning', '4 months intern'], img:'img/logos/bootstrap.png'}
       ], hardware: [
-          {name: 'ARM', exps: ['1 year learning']},
-          {name: 'Verilog', exps: ['1 year learning']},
+          {name: 'Verilog HDL / ARM', exps: ['1 year learning'], img:'img/logos/fpga.png'},
       ], system: [
-          {name: 'MYSQL', exps: ['4 months intern']},
-          {name: 'AWS', exps: ['3 months learning']}
+          {name: 'MYSQL', exps: ['4 months intern'], img:'/img/logos/mysql.png'},
+          {name: 'AWS', exps: ['3 months learning'], img:'/img/logos/aws.jpg'}
       ]}
     }
   }
 }
 </script>
+
+<style scoped>
+    .prog-logo {
+        width: 100px;
+        height: 100px;
+    }
+
+    .hd-logo {
+        width: 250px;
+        height: 100px;
+    }
+</style>
